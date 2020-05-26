3bdb26695b62000f355d3649a371ca8d6c65bff5
crazvik <crazvik@gmail.com>
2020-05-26 19:10:34 +0200

    Hopefully progress

--- src/main/java/se/ecutb/foodReview/controller/AdminController.java
+++ src/main/java/se/ecutb/foodReview/controller/AdminController.java
@@ -17,38 +17,9 @@ import javax.validation.Valid;
 
 @Controller
 public class AdminController {
-    private RestaurantRepo restaurantRepo;
-    private RestaurantService restaurantService;
-
-    @Autowired
-    public AdminController(RestaurantRepo restaurantRepo, RestaurantService restaurantService) {
-        this.restaurantRepo = restaurantRepo;
-        this.restaurantService = restaurantService;
-    }
-
     @GetMapping("/admin")
     public String getAdminPage(){
         return "admin-page";
     }
 
-    @GetMapping("admin/registerRestaurant/form")
-    public String getForm(Model model){
-        model.addAttribute("form", new CreateRestaurantForm());
-        return "registerRestaurant";
-    }
-
-    @PostMapping("admin/registerRestaurant/process")
-    public String formProcess(@Valid @ModelAttribute("form") CreateRestaurantForm form, BindingResult bindingResult) {
-        if (restaurantRepo.findByNameIgnoreCase(form.getRestaurantName()).isPresent()) {
-            FieldError error = new FieldError("form", "name", "Name is already in use");
-            bindingResult.addError(error);
-        }
-
-        if (bindingResult.hasErrors()) {
-            return "registerRestaurant";
-        }
-
-        Restaurant restaurant = restaurantService.registerRestaurant(form.getRestaurantName());
-        return "redirect:/restaurants";
-    }
 }
--- src/main/java/se/ecutb/foodReview/controller/UserController.java
+++ src/main/java/se/ecutb/foodReview/controller/UserController.java
@@ -8,30 +8,37 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import se.ecutb.foodReview.data.RestaurantRepo;
 import se.ecutb.foodReview.data.ReviewerRepo;
 import se.ecutb.foodReview.dto.CreateRestaurantForm;
 import se.ecutb.foodReview.dto.CreateReviewerForm;
+import se.ecutb.foodReview.entity.Restaurant;
 import se.ecutb.foodReview.entity.Reviewer;
 import se.ecutb.foodReview.service.RestaurantService;
 import se.ecutb.foodReview.service.ReviewerService;
 
 import javax.validation.Valid;
 import java.time.LocalDate;
+import java.util.List;
 
 @Controller
 public class UserController {
     private ReviewerRepo reviewerRepo;
     private ReviewerService reviewerService;
+    private RestaurantRepo restaurantRepo;
+    private RestaurantService restaurantService;
 
     @Autowired
-    public UserController(ReviewerRepo reviewerRepo, ReviewerService reviewerService) {
+    public UserController(ReviewerRepo reviewerRepo, ReviewerService reviewerService, RestaurantRepo restaurantRepo, RestaurantService restaurantService) {
         this.reviewerRepo = reviewerRepo;
         this.reviewerService = reviewerService;
+        this.restaurantRepo = restaurantRepo;
+        this.restaurantService = restaurantService;
     }
 
     @GetMapping("users/register/form")
-    public String getForm(Model model) {
+    public String getRegisterForm(Model model) {
         model.addAttribute("form", new CreateReviewerForm());
         return "register";
     }
@@ -62,7 +69,33 @@ public class UserController {
         }
     }
 
+    @GetMapping("admin/registerRestaurant/form")
+    public String getRestaurantForm(Model model){
+        model.addAttribute("form", new CreateRestaurantForm());
+        return "registerRestaurant";
+    }
 
+    @PostMapping("admin/registerRestaurant/process")
+    public String formProcess(@Valid @ModelAttribute("form") CreateRestaurantForm form, BindingResult bindingResult) {
+        if (restaurantRepo.findByNameIgnoreCase(form.getRestaurantName()).isPresent()) {
+            FieldError error = new FieldError("form", "name", "Name is already in use");
+            bindingResult.addError(error);
+        }
+
+        if (bindingResult.hasErrors()) {
+            return "registerRestaurant";
+        }
+
+        Restaurant restaurant = restaurantService.registerRestaurant(form.getRestaurantName());
+        return "redirect:/restaurants";
+    }
+
+    @RequestMapping("/restaurants")
+    public String restaurantList(Model model) {
+        List<Restaurant> restaurants = (List<Restaurant>) restaurantRepo.findAll();
+        model.addAttribute("restaurants", restaurants);
+        return "restaurants";
+    }
 
     @GetMapping("/login")
     public String getLoginForm(){
