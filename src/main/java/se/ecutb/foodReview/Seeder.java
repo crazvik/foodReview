29de57f654d7b727291ede7c3d2cc4c516d5c242
crazvik <crazvik@gmail.com>
2020-05-29 12:59:40 +0200

    Added more things to seeder

--- src/main/java/se/ecutb/foodReview/Seeder.java
+++ src/main/java/se/ecutb/foodReview/Seeder.java
@@ -5,6 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import se.ecutb.foodReview.data.ReviewerRoleRepo;
 import se.ecutb.foodReview.entity.ReviewerRole;
+import se.ecutb.foodReview.service.FoodItemService;
 import se.ecutb.foodReview.service.RestaurantService;
 import se.ecutb.foodReview.service.ReviewerService;
 
@@ -15,12 +16,14 @@ public class Seeder implements CommandLineRunner {
     private ReviewerService reviewerService;
     private ReviewerRoleRepo roleRepo;
     private RestaurantService restaurantService;
+    private FoodItemService foodItemService;
 
     @Autowired
-    public Seeder(ReviewerService reviewerService, ReviewerRoleRepo roleRepo, RestaurantService restaurantService) {
+    public Seeder(ReviewerService reviewerService, ReviewerRoleRepo roleRepo, RestaurantService restaurantService, FoodItemService foodItemService) {
         this.reviewerService = reviewerService;
         this.roleRepo = roleRepo;
         this.restaurantService = restaurantService;
+        this.foodItemService = foodItemService;
     }
 
     @Override
@@ -31,7 +34,11 @@ public class Seeder implements CommandLineRunner {
         reviewerService.registerReviewer("Admin", "Adminson", "Admin", "admin", LocalDate.now(), true);
         reviewerService.registerReviewer("User", "Userson", "User", "user", LocalDate.now(), false);
 
-        restaurantService.registerRestaurant("First");
-        restaurantService.registerRestaurant("Second");
+        restaurantService.registerRestaurant("FirstRestaurant");
+        restaurantService.registerRestaurant("SecondRestaurant");
+
+        foodItemService.register("ThisFoodItem", "Added food with seeder to test", "", "", 1);
+        foodItemService.register("ThatFoodItem", "Added food with seeder to test", "", "", 2);
+        foodItemService.register("AnotherFoodItem", "Added more food with seeder to test", "", "", 2);
     }
 }
